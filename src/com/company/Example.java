bcdb2dec49609348ef15669c9c2aeb11667fb7e5
hbtexus <hbtexus@gmail.com>
2019-06-18 22:12:51 -0500

    Added Hello

--- src/com/company/Example.java
+++ src/com/company/Example.java
@@ -3,7 +3,7 @@ package com.company;
 public class Example {
     public static void main(String[] args) {
 
-        String myString = "hello";
+        String myString = "Hello World";
 
         System.out.println(myString);
         System.out.println(myString.toUpperCase());
